1c0f09f8e0eeafce2b40b861b74407c963a8afcc
Olivier <olivier.cochez@gmail.com>
2014-04-27 23:58:25 +0200

    last try before going to sleep

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # These functions cache the inverse data of a matrix if it has
 # not already been calculated yet. If the inverse matrix has already
 # been computed, it return the cached value
@@ -35,20 +34,3 @@ cacheSolve <- function(x, ...)
         x$setsolve(m)
         m
 }
-=======
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
->>>>>>> 7f657dd22ac20d22698c53b23f0057e1a12c09b7
